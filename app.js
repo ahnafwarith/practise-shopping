10a14222acfd97e01a98ebfecb03558a71c82688
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-02-16 17:27:52 +0600

    got errored lol

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+function updateCase(product, isIncreasing, price) {
+    const caseInput = document.getElementById(product + '-input');
+    let caseInputValue = caseInput.value;
+    if (isIncreasing == true) {
+        caseInputValue = parseInt(caseInputValue) + 1;
+    }
+    else if (isIncreasing == false && caseInput.value > 0) {
+        caseInputValue = parseInt(caseInputValue) - 1;
+    }
+    caseInput.value = caseInputValue;
+    const caseNumber = document.getElementById(product + '-number')
+    caseNumber.innerText = caseInputValue * price;
+}
+
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateCase(phone, true, 1219);
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateCase(phone, false, 1219);
+})
+
 document.getElementById('case-plus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-input');
-    const caseInputValue = caseInput.value;
-    caseInput.value = parseInt(caseInputValue) + 1;
+    updateCase(case, true, 59);
 })
 document.getElementById('case-minus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-input');
-    if (caseInput.value >= 1) {
-        const caseInputValue = caseInput.value;
-        caseInput.value = parseInt(caseInputValue) - 1;
-    }
+    updateCase(case, false, 59);
 })
